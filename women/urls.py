9a8d4084027a2cb6038ee1dd50d6909bf9253268
Roman Kondratiuk <roma.kondratiuk@icloud.com>
2023-10-31 18:23:37 -0400

    собственные конверторы

--- women/urls.py
+++ women/urls.py
@@ -1,8 +1,15 @@
-from django.urls import path
+from django.urls import path, re_path, register_converter
 
 from . import views
+from . import converters
+
+register_converter(converters.FourDigitYearConverter, "year4")
+
 
 urlpatterns = [
     path('', views.index),  # http://127.0.0.1:8000
-    path('category/', views.categories),  # http://127.0.0.1:8000/category/
+    path('category/<int:category_id>/', views.categories),  # http://127.0.0.1:8000/category/1/
+    path('category/<slug:category_slug>/', views.categories_by_slug),  # http://127.0.0.1:8000/category/1/
+    # re_path(r"^archive/(?P<year>[0-9]{4})/", views.archive) # собственные конверторы
+    path("archive/<year4:year>/", views.archive), # собственные конверторы
 ]
--- women/views.py
+++ women/views.py
@@ -6,5 +6,13 @@ def index(request):
     return HttpResponse("This is page for women")
 
 
-def categories(request):
-    return HttpResponse('<h1>"Articles by category"</h1>')
+def categories(request, category_id):
+    return HttpResponse(f'<h1>Articles by category</h1><p>id: {category_id}</p>')
+
+
+def categories_by_slug(request, category_slug):
+    return HttpResponse(f'<h1>Articles by category</h1><p>id: {category_slug}</p>')
+
+
+def archive(request, year):
+    return HttpResponse(f'<h1>Archive by years</h1><p>{year}</p>')
