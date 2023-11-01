0f0b261ac20a497c0944898cfe40f68298f39ed0
Roman Kondratiuk <roma.kondratiuk@icloud.com>
2023-10-31 22:06:38 -0400

    Exception Handling

--- women/views.py
+++ women/views.py
@@ -1,5 +1,10 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render
+
+
+def page_not_found(request, exception):
+    """ function for not found pages """
+    return HttpResponseNotFound("<h1> Page Not Found !!! </h1>")
 
 
 def index(request):
@@ -11,8 +16,12 @@ def categories(request, category_id):
 
 
 def categories_by_slug(request, category_slug):
+    if request.GET:
+        print(request.GET)
     return HttpResponse(f'<h1>Articles by category</h1><p>id: {category_slug}</p>')
 
 
 def archive(request, year):
+    if year > 2023:
+        raise Http404()
     return HttpResponse(f'<h1>Archive by years</h1><p>{year}</p>')
